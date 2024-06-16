332febdee9564d81d4910831682cd55ddad38dcd
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-06-17 00:40:14 +0900

    button OK

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log('Hello, World!');
+
+
+function profile_contents(aa) {
+    console.log(aa);
+    if (aa == 0) {
+        $("#profile_contents").css("top", "0");
+        aa = 1;
+    }
+    else {
+        $("#profile_contents").css("top", "100%");
+        aa = 0;
+    }
+}
+
+function work_contents(aa) {
+    console.log(aa);
+    if (aa == 0) {
+        $("#work_contents").css("top", "0");
+        aa = 1;
+    }
+    else {
+        $("#work_contents").css("top", "100%");
+        aa = 0;
+    }
+}
+
+function skill_contents(aa) {
+    if (aa == 0) {
+        $("#skill_contents").css("top", "0");
+        aa = 1;
+    }
+    else {
+        $("#skill_contents").css("top", "100%");
+        aa = 0;
+    }
+}
+
+function contact_contents(aa) {
+    console.log(aa);
+    if (aa == 0) {
+        $("#contact_contents").css("top", "0");
+        aa = 1;
+    }
+    else {
+        $("#contact_contents").css("top", "100%");
+        aa = 0;
+    }
+}
+
+function konkon_contents(aa) {
+    console.log(aa);
+    if (aa == 0) {
+        $("#konkon_contents").css("top", "0");
+        aa = 1;
+    }
+    else {
+        $("#konkon_contents").css("top", "100%");
+        aa = 0;
+    }
+}
