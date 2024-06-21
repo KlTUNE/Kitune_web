77ab59882f40c513b2bd675bf8754b1e1df4d332
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-06-21 17:59:26 +0900

    skill

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-function profile_contents(aa) {
-    console.log(aa);
-    if (aa == 0) {
+function profile_contents(open_flag) {
+    console.log(open_flag);
+    if (open_flag == 0) {
         $("#profile_contents").css("top", "0");
-        aa = 1;
+        open_flag = 1;
     }
     else {
         $("#profile_contents").css("top", "100%");
-        aa = 0;
+        open_flag = 0;
     }
 }
 
-function work_contents(aa) {
-    console.log(aa);
-    if (aa == 0) {
+function work_contents(open_flag) {
+    console.log(open_flag);
+    if (open_flag == 0) {
         $("#work_contents").css("top", "0");
-        aa = 1;
+        open_flag = 1;
     }
     else {
         $("#work_contents").css("top", "100%");
-        aa = 0;
+        open_flag = 0;
     }
 }
 
-function skill_contents(aa) {
-    if (aa == 0) {
+function skill_contents(open_flag) {
+    if (open_flag == 0) {
         $("#skill_contents").css("top", "0");
-        aa = 1;
+        open_flag = 1;
     }
     else {
         $("#skill_contents").css("top", "100%");
-        aa = 0;
+        open_flag = 0;
     }
 }
 
-function contact_contents(aa) {
-    console.log(aa);
-    if (aa == 0) {
+function contact_contents(open_flag) {
+    console.log(open_flag);
+    if (open_flag == 0) {
         $("#contact_contents").css("top", "0");
-        aa = 1;
+        open_flag = 1;
     }
     else {
         $("#contact_contents").css("top", "100%");
-        aa = 0;
+        open_flag = 0;
     }
 }
 
-function konkon_contents(aa) {
-    console.log(aa);
-    if (aa == 0) {
+function konkon_contents(open_flag) {
+    console.log(open_flag);
+    if (open_flag == 0) {
         $("#konkon_contents").css("top", "0");
-        aa = 1;
+        open_flag = 1;
     }
     else {
         $("#konkon_contents").css("top", "100%");
-        aa = 0;
+        open_flag = 0;
     }
 }
